3ffc3f4f2475edb5638fe138e69f2921f25cec92
Guilherme Campano <guilherme.campano@concretesolutions.com.br>
2015-07-20 14:09:13 -0300

    add list
    Adicionar item na lista principal

--- www/app/views/list/ListDetailCtrl.js
+++ www/app/views/list/ListDetailCtrl.js
@@ -1,4 +1,10 @@
 controllers.controller('ListDetailCtrl', function($scope, $stateParams, Lists) {
   $scope.list = Lists.get($stateParams.listId);
+    
+  $scope.removeItem = function(listId,item) {
+      Lists.removeItem(listId,item);
+  };
+    
+
 });
 
--- www/app/views/list/ListService.js
+++ www/app/views/list/ListService.js
@@ -2,40 +2,54 @@ services.factory('Lists', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var lists = [{
+  var lists = [
+  {
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
+    name: 'Mercado',
+    description: 'LAAAAGANHA',
+    image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',
+    dateOfCreation: '18/05/2015',
+    items: [
+        {id: 0, name: "Massa", qtd:1, unity:""},
+        {id: 1, name: "Molho de tomate", qtd:2, unity:"Latas"},
+        {id: 2, name: "Queijo", qtd:300, unity:"gramas"},
+        {id: 3, name: "Presunto", qtd:500, unity:"gramas"},
+    ]
+  },
+               
+  {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    name: 'Shopping',
+    description: 'Tranqueiras',
+    image: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460',
+    dateOfCreation: '20/05/2015',
+    items: [
+        {id: 0, name: "Capinha do E5", qtd:1, unity:""},
+        {id: 1, name: "Xerox", qtd:5, unity:"cópias"},  
+    ]
+  }
+               
+  ];
 
   return {
     all: function() {
       return lists;
     },
+    
+    add: function(list) {
+      if(lists.push(list)) return true;
+      else return false;
+    },
+      
     remove: function(list) {
       lists.splice(lists.indexOf(list), 1);
     },
+      
+    removeItem: function(listId, item) {
+      var items = lists[listId].items;
+      lists[listId].items.splice(items.indexOf(item), 1);
+    },
+      
     get: function(listId) {
       for (var i = 0; i < lists.length; i++) {
         if (lists[i].id === parseInt(listId)) {
