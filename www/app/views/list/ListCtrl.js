1f28c32773d964f17c4498d2eef541da61b0ce59
guilherme.campano <guilherme.campano@concretesolutions.com.br>
2015-07-23 11:35:59 -0300

    validar nome da lista

--- www/app/views/list/ListCtrl.js
+++ www/app/views/list/ListCtrl.js
@@ -2,21 +2,34 @@
 controllers.controller('ListCtrl', function ($scope, Lists, $state) {
 
     $scope.lists = Lists.all();
+    $scope.msgErr = false;
     
-
     $scope.newList = {
+        
         id : $scope.lists.length ,
         name: '',
         description: '',
         image: '',
         dateOfCreation: new Date().getTime(),
         items : [],
+    
+        
         add : function (){
+            if ( !this.validate() ) return false; 
             var msg = Lists.add(this);
             if (msg) $state.transitionTo('tab.list');
-        }
-       
+        },
+        
+        validate : function (){
+            if ( this.name.length <=0 ){
+                $scope.msgErr = "Nome da lista não pode ser vazia";
+                return false;
+            }
+            $scope.msgErr = false;
+            return true;
+        },
     }
+    
     
     $scope.remove = function (list) {
         Lists.remove(list);
